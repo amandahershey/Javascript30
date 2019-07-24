a542ed9f42316f687d73db47d5474165fcababe8
Amanda <acbhershey@gmail.com>
2019-07-24 19:59:27 -0400

    Created rgb split filter and "ghost effect"

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -33,8 +33,10 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     // take pixels out of img
     let pixels = ctx.getImageData(0, 0, width, height);
-    // alter pixels
-    pixels = redEffect(pixels);
+    // altered pixels (filters)
+    //pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.1;
     // display altered pixels
     ctx.putImageData(pixels, 0, 0);
   }, 16);
@@ -60,6 +62,15 @@ function redEffect(pixels) {
     pixels.data[i + 0] += 100;
     pixels.data[i + 1] -= 50;
     pixels.data[i + 2] *= 0.5;
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 550] = pixels.data[i + 0]; // red
+    pixels.data[i + 500] = pixels.data[i + 1]; // green
+    pixels.data[i - 550] = pixels.data[i + 2]; // blue
   }
   return pixels;
 }
