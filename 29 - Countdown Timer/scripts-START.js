caed517816076c915d82a7b93bf51f7a99f31ef4
Amanda <acbhershey@gmail.com>
2019-07-25 14:59:16 -0400

    Countdown converted to hours, minutes, and seconds

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -17,5 +17,10 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
-  console.log(seconds);
+  const minutes = Math.floor(seconds / 60);
+  const remainderSeconds = seconds % 60;
+  console.log({
+    minutes,
+    remainderSeconds
+  });
 }
