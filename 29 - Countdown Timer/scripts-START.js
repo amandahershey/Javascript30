99df277e569bba6d62ae3f6a03c9b724e8561175
Amanda <acbhershey@gmail.com>
2019-07-25 15:11:43 -0400

    h1 is now displaying time with 2 digits for hours, minutes, and seconds

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds) {
   const now = Date.now();
@@ -17,10 +18,10 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  seconds = seconds % 3600;
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  console.log({
-    minutes,
-    remainderSeconds
-  });
+  const display = `${hours < 10 ? '0' : ''}${hours}:${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  timerDisplay.textContent = display;
 }
